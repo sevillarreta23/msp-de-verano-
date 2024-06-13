96bc90f8699c4e82b7f7812447633cd9f3723b12
sevillarreta23 <jose.sevillagames23@gmail.com>
2024-06-13 17:26:31 +0000

    Mundo guardado 2024-06-13

--- servidor.py
+++ servidor.py
@@ -38,6 +38,7 @@
 
 
 
+
 import requests,os,base64
 if not os.path.exists("./.gitignore"):
 	big = "L3dvcmtfYXJlYQ0KL3NlcnZpZG9yX21pbmVjcmFmdA0KL21pbmVjcmFmdF9zZXJ2ZXINCi9zZXJ2aWRvcl9taW5lY3JhZnRfb2xkDQovdGFpbHNjYWxlLWNzDQovdGhhbm9zDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioub3V0cHV0"
